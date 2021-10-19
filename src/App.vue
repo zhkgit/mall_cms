cfe39296b397a1e904db8eb8de536f051d96da51
hua_kun <1140883907@qq.com>
2021-10-19 12:32:15 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,20 @@
     <router-link to="/about">About</router-link>
   </div>
   <router-view />
+  
 </template>
+
 
 <style lang="less">
 #app {
+
+
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+
   text-align: center;
+
   color: #2c3e50;
 }
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,12 +6,15 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
+import { defineComponent } from 'vue';
+import HelloWorld from '../components/HelloWorld.vue'; // @ is an alias to /src
 
 export default defineComponent({
   name: 'Home',
   components: {
+
+
+
     HelloWorld
   }
 })
